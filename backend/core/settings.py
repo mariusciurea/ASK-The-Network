a5f999c358e29c5a3dd86141cd5e7ddf4ddf70a4
mariusciurea <ciurea.marius1@gmail.com>
2026-07-03 11:19:19 +0300

    updated model

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -17,7 +17,7 @@ class Settings(BaseSettings):
     DB_URL:str ="mysql+pymysql://root:Changeme_123@localhost:3306/radio_network_data"
 
     # model
-    MODEL_NAME: str = "gemini-3-flash-preview"
+    MODEL_NAME: str = "gemini-2.5-flash"
     GOOGLE_API_KEY: str = ""
 
     # MCP
